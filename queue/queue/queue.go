781da7b3ced3ebcdc7deb37cc26a456732d7e7a6
Masoud Heydari <m.heydari4883@gmail.com>
2022-02-09 17:26:37 +0330

    add queue Enqueue() function

--- queue/queue/queue.go
+++ queue/queue/queue.go
@@ -2,8 +2,8 @@ package queue
 
 type (
 	Queue struct {
-		head   *node
-		length int
+		head, tail *node
+		length     int
 	}
 
 	node struct {
@@ -11,6 +11,25 @@ type (
 		next  *node
 	}
 )
+
+// Enqueue adds a new node at the end of queue
+func (slf *Queue) Enqueue(newValue interface{}) {
+	newNode := &node{
+		value: newValue,
+		next:  nil,
+	}
+	if slf.length == 0 {
+		// queue is empty
+		slf.head = newNode
+		slf.tail = newNode
+	} else {
+		// queue has at least one node
+		slf.tail.next = newNode
+		slf.tail = newNode
+	}
+
+	slf.length++
+}
 
 // Size return the size of queue
 func (slf *Queue) Size() int {
